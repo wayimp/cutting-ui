3913a7507e02784a750f263b12d92aca7e105950
Keith Humphrey <keith@plymouthcommercialservices.com>
2020-11-07 11:29:02 -0600

    changes for deploy
    Signed-off-by: Keith Humphrey <keith@plymouthcommercialservices.com>

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4040'
-    //baseURL: 'https://api.valleycuttingsystems.net'
+    // baseURL: 'http://localhost:4040'
+    baseURL: 'https://api.valleycuttingsystems.net'
 });
 
 export default instance;
